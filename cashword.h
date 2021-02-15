35efee6f92199b8b8cb4185f857fd58ae4b68d07
Jason Davis <36575784+Synerin@users.noreply.github.com>
2021-02-15 14:44:23 -0500

    Update word matching
    See cashword.c for description

--- cashword.h
+++ cashword.h
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #define MAX_WORD_LENGTH 11 // The length of the Cashword grid
 
-int cash_word(char * letters);
-int valid_word(char * letters, char * word);
+// ListNode structure for word tracking
+struct ListNode {
+    char* word;
+    struct ListNode* next;
+};
+
+// Functions
+struct ListNode* cash_word(char* letters);
+int valid_word(char* letters, char* word, struct ListNode* root);
 int prize_value(int wordsFound);
 int prize_mult(int wordsFound, int multiplier);
+int count_nodes(struct ListNode* wordsFound);
+void print_nodes(struct ListNode* wordsFound);
